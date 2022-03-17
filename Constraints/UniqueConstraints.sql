8fb1f2c691cc085b1f1c08d8c18c4aeab39f097a
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-17 09:24:49 -0400

    Update UniqueConstraints.sql

--- Constraints/UniqueConstraints.sql
+++ Constraints/UniqueConstraints.sql
@@ -1,1 +1,18 @@
+1. Adding UNIQUE constraint using ALTER TABLE syntax:
 
+ALTER TABLE attendees 
+ADD UNIQUE (email);
+
+ALTER TABLE talks
+ADD UNIQUE (speaker_id, session_timeslot);
+
+ALTER TABLE speakers
+ADD UNIQUE (email);
+
+CREATE TABLE registrations (
+    id integer NOT NULL,
+    attendee_id integer NOT NULL,
+    session_timeslot timestamp NOT NULL,
+    talk_id integer NOT NULL,
+    UNIQUE (attendee_id, session_timeslot)
+);
