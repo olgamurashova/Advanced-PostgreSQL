cf37a1afb43ea6de8aa9ce8271e183f0b9709721
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-17 04:18:45 -0400

    Update NullabilityConstraints.sql

--- Constraints/NullabilityConstraints.sql
+++ Constraints/NullabilityConstraints.sql
@@ -1,3 +1,5 @@
+1. Creating the tables and adding NOT NULL constraints:
+
 CREATE TABLE talks (
     id integer,
     title varchar NOT NULL,
@@ -14,3 +16,13 @@ organization varchar,
 title varchar,
 years_in_role integer
 );
+
+2. Altering the tables and adding constraints:
+
+ALTER TABLE talks
+ALTER COLUMN session_timeslot SET NOT NULL;
+
+3. Removing contsraint:
+
+ALTER TABLE talks
+ALTER COLUMN session_timeslot DROP NOT NULL;
