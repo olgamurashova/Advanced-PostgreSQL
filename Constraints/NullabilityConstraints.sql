75f63da7c72d9592b19187345b746871da102a13
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-17 04:06:08 -0400

    Update NullabilityConstraints.sql

--- Constraints/NullabilityConstraints.sql
+++ Constraints/NullabilityConstraints.sql
@@ -5,3 +5,12 @@ CREATE TABLE talks (
     estimated_length integer,
     session_timeslot timestamp NOT NULL
 );
+
+CREATE TABLE speakers (
+id integer NOT NULL,
+email varchar NOT NULL,
+name varchar NOT NULL,
+organization varchar,
+title varchar,
+years_in_role integer
+);
